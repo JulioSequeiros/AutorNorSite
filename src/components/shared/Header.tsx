6bee876d0a9382bc940e733a72b17bdd876df5d2
Júlio Sequeiros <juliosequeiros@ipvc.pt>
2024-01-05 14:59:53 +0000

    first commmit

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header className="pb-3 mb-4 border-bottom">
             <a href="/src/components/public" className="d-flex align-items-center text-dark text-decoration-none">
-                <img src="/logo-estg.svg" alt="ESTG" className="mw-100" />
+                <img src="/logo-autonor.svg" alt="ESTG" className="mw-100" width="200" />
             </a>
         </header>
     );
