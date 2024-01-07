3519d17a764641749bb215a11371b84dd1cde5cc
X-ERR0R-X <duartegomes@ipvc.pt>
2024-01-07 19:30:02 +0000

    -

---
+++ src/services/manutencoes.service.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const API_URL = "https://gestao-viaturas-manutencao.vercel.app/api/viaturas";
+
+axios.interceptors.request.use(
+    config => {
+        config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+const getById = (number) => {
+    return axios.get(API_URL + number);
+};
+
+const createORupdate = (id, descricao, data, custo, viatura, viaturaId, categoria, categoriaId) => {
+    if(id == null){
+        return create(descricao, data, custo, viatura, viaturaId, categoria, categoriaId);
+    }
+    else {
+        return update(id, descricao, data, custo, viatura, viaturaId, categoria, categoriaId);
+    }
+};
+
+const create = (descricao, data, custo, viatura, viaturaId, categoria, categoriaId) => {
+    return axios.post(API_URL + "create", { descricao, data, custo, viatura, viaturaId, categoria, categoriaId });
+};
+
+const update = (id, descricao, data, custo, viatura, viaturaId, categoria, categoriaId) => {
+    return axios.put(API_URL + "update", { id, descricao, data, custo, viatura, viaturaId, categoria, categoriaId });
+};
+
+const deleteUser = (id) => {
+    return axios.delete(API_URL + "delete/" + id);
+};
+
+const ManutencoesService = {
+    getById,
+    createORupdate,
+    create,
+    update,
+    deleteUser
+}
+
+export default ManutencoesService;
---
+++ src/services/marcacoes.service.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const API_URL = "https://gestao-viaturas-manutencao.vercel.app/api/viaturas";
+
+axios.interceptors.request.use(
+    config => {
+        config.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+const getById = (number) => {
+    return axios.get(API_URL + number);
+};
+
+const createORupdate = (id, data, descricao, viatura, viaturaId) => {
+    if(id == null){
+        return create(data, descricao, viatura, viaturaId);
+    }
+    else {
+        return update(id, data, descricao, viatura, viaturaId);
+    }
+};
+
+const create = (data, descricao, viatura, viaturaId) => {
+    return axios.post(API_URL + "create", { data, descricao, viatura, viaturaId });
+};
+
+const update = (id, data, descricao, viatura, viaturaId) => {
+    return axios.put(API_URL + "update", { id, data, descricao, viatura, viaturaId });
+};
+
+const deleteUser = (id) => {
+    return axios.delete(API_URL + "delete/" + id);
+};
+
+const MarcacoesService = {
+    getById,
+    createORupdate,
+    create,
+    update,
+    deleteUser
+}
+
+export default MarcacoesService;
